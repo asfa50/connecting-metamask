42652d0b2b74084244b4b2e88f6d850562d82c2e
asfa50 <alexandergrin1@hotmail.com>
2023-10-15 17:34:17 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import './App.css';
 import React, {useState} from 'react';
-import {Web3} from "web3";
+import { Web3 } from 'web3';
 
 function App() {
-
-    const [walletAddress, setWalletAddress] = useState("")
+  const [walletAddress, setWalletAddress] = useState("");
+  const web3 = new Web3('https://mainnet.infura.io/v3/438f0c4eb4ab46168d928e77ca363c0f');
+  const [balanceInEther] = useState("")
 
     async function requestAccount() {
         console.log('Requesting account...')
-
         if(window.ethereum) {
             console.log('Metamask Wallet Detected')
             try {
-                const accounts = await window.ethereum.request({
-                    method: "eth_requestAccounts",
-                });
+                const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
                 setWalletAddress(accounts[0])
-                Web3.eth.getBalance(walletAddress)
+                console.log(walletAddress)
                 }
             catch(error) {
                 console.log("Error connecting Wallet")
@@ -27,20 +25,21 @@ function App() {
         }
     }
 
-    async function connectWallet(){
-        if(typeof window.ethereum !== 'undefined') {
-            console.log('Metamask Detected')
-    }}
+    async function getBalance() {
+        const balanceInWei = await web3.eth.getBalance(walletAddress);
+        const balanceInEther = web3.utils.fromWei(balanceInWei, 'ether');
+        console.log(`${Number(balanceInEther)} Ether`);
+    }
 
     return (
     <div className="App">
       <header className="App-header">
        <button
-
        onClick={requestAccount}
-
        >Connect Metamask Wallet</button>
         <h3>Wallet Address: {walletAddress}</h3>
+          <button onClick={getBalance}>Get Account Balance</button>
+          <h2>Wallet Balance: {Number(balanceInEther)} </h2>
       </header>
     </div>
   );
